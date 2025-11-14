80ed631b8e168e009c15de3afd0beb099f5f7a0b
Мирхайдарова Диана <lamyrr@bk.ru>
2025-11-14 19:57:57 +0500

    Создаем класс-модель для хранения трех чисел A, B, C с гарантией выполнения правила A <= B <= C.

---
+++ triplemodel.h
@@ -0,0 +1,37 @@
+﻿#ifndef TRIPLEMODEL_H
+#define TRIPLEMODEL_H
+
+#include <QObject>
+#include <QSettings>
+
+class TripleModel : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit TripleModel(QObject* parent = nullptr);
+
+    int a() const;
+    int b() const;
+    int c() const;
+
+    void setA(int newA);
+    void setB(int newB);
+    void setC(int newC);
+
+    void saveState();
+    void loadState();
+
+signals:
+    void dataChanged();
+    void valueRejected();
+
+private:
+    int m_a;
+    int m_b;
+    int m_c;
+
+    void normalizeValues();
+};
+
+#endif // TRIPLEMODEL_H
