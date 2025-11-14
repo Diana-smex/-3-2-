9a49038ae7c86a13f3c0f0019f91353b6d6054be
Мирхайдарова Диана <lamyrr@bk.ru>
2025-11-14 19:59:23 +0500

    создаем главное окно с тремя строками элементов управления (текстовое поле, счётчик и ползунок) для каждого числа A, B и C, которые синхронизируются с моделью.

--- QtWidgetsApplication.h
+++ QtWidgetsApplication.h
@@ -1,17 +1,47 @@
-#pragma once
+﻿#pragma once
 
 #include <QtWidgets/QMainWindow>
-#include "ui_QtWidgetsApplication.h"
+#include "triplemodel.h"
+
+class QLineEdit;
+class QSpinBox;
+class QSlider;
+class QLabel;
+class QVBoxLayout;
+class QHBoxLayout;
 
 class QtWidgetsApplication : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    QtWidgetsApplication(QWidget *parent = nullptr);
+    QtWidgetsApplication(QWidget* parent = nullptr);
     ~QtWidgetsApplication();
 
-private:
-    Ui::QtWidgetsApplicationClass ui;
-};
+private slots:
+    void onAValueChanged();
+    void onBValueChanged();
+    void onCValueChanged();
+    void onValueRejected();
 
+    void onModelDataChanged();
+
+private:
+    void createUI();
+    QHBoxLayout* createRowForValue(const QString& label, QLineEdit*& lineEdit, QSpinBox*& spinBox, QSlider*& slider);
+    void blockWidgetSignals(bool block);
+
+    TripleModel* m_model;
+
+    QLineEdit* m_lineEditA;
+    QSpinBox* m_spinBoxA;
+    QSlider* m_sliderA;
+
+    QLineEdit* m_lineEditB;
+    QSpinBox* m_spinBoxB;
+    QSlider* m_sliderB;
+
+    QLineEdit* m_lineEditC;
+    QSpinBox* m_spinBoxC;
+    QSlider* m_sliderC;
+};
